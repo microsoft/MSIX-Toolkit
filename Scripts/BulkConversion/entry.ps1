f632ff54c71e179ae399bcd60f95518633acaa65
RDMacLachlan <roy.maclachlan@microsoft.com>
2020-04-27 14:05:35 -0700

    Update entry.ps1

--- Scripts/BulkConversion/entry.ps1
+++ Scripts/BulkConversion/entry.ps1
@@ -1,20 +1,26 @@
 . $PSScriptRoot\bulk_convert.ps1
 . $PSScriptRoot\SharedScriptLib.ps1
 
-##
-## Usage:
-##
+## Retrieves the credentials that will be used for connecting to both Virtual and Remote Machines. Credentials must be consistent.
 $credential = Get-Credential
 
+## The Code Signing certificate to be used for signing the MSIX Apps.
+$SigningCertificate = @{
+    Password = "P@ssw0rd"; Path = "C:\Temp\cert.pfx"
+}
+
+## Virtual Machines to be used for converting applications to the MSIX Packaging format.
 $virtualMachines = @(
     @{ Name = "vm1"; Credential = $credential }
     @{ Name = "vm2"; Credential = $credential }
 )
 
+## Remote Machines to be used for converting applications to the MSIX Packaging format.
 $remoteMachines = @(
     @{ ComputerName = "YourVMNameHere.westus.cloudapp.azure.com"; Credential = $credential }
 )
 
+## Applications to be converted.
 $conversionsParameters = @(
     @{
         InstallerPath = "Path\To\Your\Installer\YourInstaller.msi";
@@ -44,6 +50,8 @@ $conversionsParameters = @(
 
 $workingDirectory = [System.IO.Path]::Combine($PSScriptRoot, "out")
 
+## Converts the identified applications to MSIX Packaging Format.
 RunConversionJobs -conversionsParameters $conversionsParameters -virtualMachines $virtualMachines -remoteMachines $remoteMachines $workingDirectory
 
-Set-MSIXSignApp "$workingDirectory\MSIX"
+## Signs the previously created MSIX Apps with provided certificate.
+Set-MSIXSignApp -msixFolder "$workingDirectory\MSIX" -CertificatePath $SigningCertificate.Path -CertificatePassword $SigningCertificate.Password
