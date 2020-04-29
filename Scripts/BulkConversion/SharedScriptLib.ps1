e0414520092ca2063905cd19ada2aaf42c02546b
RDMacLachlan <roy.maclachlan@microsoft.com>
2020-04-28 22:59:24 -0700

    Update SharedScriptLib.ps1

--- Scripts/BulkConversion/SharedScriptLib.ps1
+++ Scripts/BulkConversion/SharedScriptLib.ps1
@@ -8,13 +8,26 @@ Param(
                             [string]  $Path      = $($PSScriptRoot + "\Log")
 )
     IF(!(Test-path -Path $Path)) {$Scratch = mkdir $Path}
+    $Error.Clear()
+
     #Formats the values required to enter for Trace32 Format
     #$TimeZoneBias = Get-WmiObject -Query "Select Bias from Win32_TimeZone"
     [string]$Time = Get-Date -Format "HH:mm:ss.ffff"
     [string]$Date = Get-Date -Format "MM-dd-yyyy"
 
     #Appends the newest log entry to the end of the log file in a Trace32 Formatting
-    $('<![LOG['+$LogValue+']LOG]!><time="'+$Time+'" date="'+$Date+'" component="'+$component+'" context="Empty" type="'+$severity+'" thread="Empty" file="'+"Empty"+'">') | out-file -FilePath $($Path+"\BulkConversion.log") -Append -NoClobber -encoding default
+    $('<![LOG['+$LogValue+']LOG]!><time="'+$Time+'" date="'+$Date+'" component="'+$component+'" context="Empty" type="'+$severity+'" thread="Empty" file="'+"Empty"+'">') | out-file -FilePath $($Path+"\BulkConversion.log") -Append -NoClobber -encoding default -ErrorAction SilentlyContinue
+
+    ## If Writing to log file fails try again.
+    While ($($Error[0].toString()).Contains("The process cannot access the file") -and $($Error[0].toString()).Contains("because it is being used by another process."))
+    {
+        ## Gives a random amount of time to wait until next write attempt.
+        $Error.Clear()
+        Sleep($(get-random -Maximum 0.5 -Minimum 0.0))
+
+        #Appends the newest log entry to the end of the log file in a Trace32 Formatting
+        $('<![LOG['+$LogValue+']LOG]!><time="'+$Time+'" date="'+$Date+'" component="'+$component+'" context="Empty" type="'+$severity+'" thread="Empty" file="'+"Empty"+'">') | out-file -FilePath $($Path+"\BulkConversion.log") -Append -NoClobber -encoding default -ErrorAction SilentlyContinue        
+    }
 
     IF($WriteHost)
     {
@@ -32,7 +45,7 @@ Function New-InitialSnapshot ($SnapshotName, $VMName, $jobId="" )
     Else
     {
         New-LogEntry -LogValue "Snapshot ($SnapshotName) for VM ($VMName) already exists. " -Component "run_job.ps1:$jobId"
-        }
+    }
 }
 
 Function Restore-InitialSnapshot ($SnapshotName, $VMName, $jobId="" )
@@ -42,4 +55,23 @@ Function Restore-InitialSnapshot ($SnapshotName, $VMName, $jobId="" )
         New-LogEntry -LogValue "Reverting Virtual Machine to earlier snapshot ($initialSnapshotName)" -Component "run_job.ps1:$jobId"
         $Scratch = Restore-VMSnapshot -Name "$SnapshotName" -VMName $vmName -Confirm:$false
     }
+}
+
+Function Set-JobProgress ($ConversionJobs, $TotalTasks)
+{
+    foreach ($job in $ConversionJobs)
+    {
+        If($job.State -ne "Running")
+            { Write-Progress -ID $job.id -Activity $job.Name -Status "Completed" -Completed -ParentID 0 }
+        Else
+            { Write-Progress -ID $job.id -Activity $job.Name -Status "Converting Application..." -PercentComplete -1 -ParentID 0 }
+    }
+
+    $RunningJobs = $($($ConversionJobs | where-object State -eq "Running").count)/2
+    $CompletedJobs = $($($ConversionJobs | where-object State -ne "Running").count)
+
+    If($($ConversionJobs | Where-object State -ne "Running").Count -eq $TotalTasks)
+        { Write-Progress -ID 0 -Status "Completed" -Completed -Activity "Capture" }
+    Else
+        { Write-Progress -ID 0 -Status "Converting Applications..." -PercentComplete $($($($RunningJobs + $CompletedJobs)/$TotalTasks)*100) -Activity "Capture" }
 }
