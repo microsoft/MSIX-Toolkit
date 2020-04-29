136d5fdee0713f9c1d5fb84f04ad04e6fd10b9f4
RDMacLachlan <roy.maclachlan@microsoft.com>
2020-04-29 01:08:00 -0700

    Update run_job.ps1

--- Scripts/BulkConversion/run_job.ps1
+++ Scripts/BulkConversion/run_job.ps1
@@ -9,7 +9,7 @@ Else
 
 $Scratch = ""
 $TemplateFile = [xml](get-content $($templateFilePath))
-New-LogEntry -LogValue "JOB: $jobId - $($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)" -Component "run_job.ps1:$jobId"
+New-LogEntry -LogValue "JOB: $($jobId+1) - $($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)" -Component "run_job.ps1:$($jobId+1)"
 
 ## Validates that the conversion machine is a Virtual Machine.
 if ($vmName)
@@ -20,24 +20,23 @@ if ($vmName)
 
 ## Reads the Template file, and logs which application is being attempted.
 $Scratch +=  "`nInitiating capture of $($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName).`n"
-New-LogEntry -LogValue "Initiating capture of $($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)." -Component "run_job.ps1:$jobId"
+New-LogEntry -LogValue "Initiating capture of $($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)." -Component "run_job.ps1:$($jobId+1)"
 
 try
 {
+
+    New-LogEntry -LogValue "MsixPackagingTool.exe create-package --template $templateFilePath --machinePassword $machinePassword" -Component "run_job.ps1:$($jobId+1)"
     ## Convert application to the MSIX Packaging format.
     foreach($Entry in $(MsixPackagingTool.exe create-package --template $templateFilePath --machinePassword $machinePassword))
-        {
-            Write-host $Entry
-            $Scratch += $Entry + "`n`r"
-        }
+    {
+        Write-host $Entry
+        $Scratch += $Entry + "`n`r"
+    }
+
     If ($Error)
-    {
-        New-LogEntry -LogValue "$Scratch" -Component "run_job.ps1:$jobId" -WriteHost $false -Severity 3
-    }
+        { New-LogEntry -LogValue "$Scratch" -Component "run_job.ps1:$($jobId+1)" -WriteHost $false -Severity 3 }
     Else
-    {
-        New-LogEntry -LogValue "$Scratch" -Component "run_job.ps1:$jobId" -WriteHost $false
-    }
+        { New-LogEntry -LogValue "$Scratch" -Component "run_job.ps1:$($jobId+1)" -WriteHost $false }
     
 }
 Catch{}
@@ -46,7 +45,7 @@ Catch{}
 if ($vmName)
 {
     ## Restores the VM to pre-app installation setting
-    Restore-InitialSnapshot -SnapshotName $initialSnapshotName -vmName $vmName -jobId $jobId
+    Restore-InitialSnapshot -SnapshotName $initialSnapshotName -vmName $vmName -jobId $($jobId+1)
 
     ## If this is a VM that can be re-used, release the global semaphore after creating a semaphore handle for this process scope
     $semaphore = New-Object -TypeName System.Threading.Semaphore -ArgumentList @($vmsCount, $vmsCount, "Global\MPTBatchConversion")
@@ -55,5 +54,5 @@ if ($vmName)
 }
 
 #Read-Host -Prompt 'Press any key to exit this window '
-New-LogEntry -LogValue "Conversion of application ($($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)) completed" -Component "run_job.ps1:$jobId"
+New-LogEntry -LogValue "Conversion of application ($($TemplateFile.MsixPackagingToolTemplate.PackageInformation.PackageDisplayName)) completed" -Component "run_job.ps1:$($jobId+1)"
 Return $Scratch
