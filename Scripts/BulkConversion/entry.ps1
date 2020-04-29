54208db4a6f070c80e20dd7b4b79de62dbcc23a1
RDMacLachlan <roy.maclachlan@microsoft.com>
2020-04-29 01:18:39 -0700

    Update entry.ps1

--- Scripts/BulkConversion/entry.ps1
+++ Scripts/BulkConversion/entry.ps1
@@ -28,40 +28,39 @@ $remoteMachines = @(
 ## Applications to be converted.
 $conversionsParameters = @(
     @{
-        PackageName = "YourApp2";
-        PackageDisplayName = "Your App2";
-        PublisherName = "CN=YourCompany";
+        PackageName          = "YourApp1";
+        PackageDisplayName   = "Your App1";
+        PublisherName        = "CN=YourCompany";
         PublisherDisplayName = "YourCompany";
-        PackageVersion = "1.0.0.0"
-        Installers = @{
-            InstallerPath = "Path\To\Your\Installer\YourInstaller2.msi"
+        PackageVersion       = "1.0.0.0";
+        InstallerPath        = "Path\to\YourInstaller1.msi";
      },
     @{
-       PackageName = "YourApp2";
-       PackageDisplayName = "Your App2";
-       PublisherName = "CN=YourCompany";
-       PublisherDisplayName = "YourCompany";
-       PackageVersion = "1.0.0.0"
-       Installers = @{
-           InstallerPath = "Path\To\Your\Installer\YourInstaller2.msi"
-}
-   },
-    @{
-        PackageName             = "YourApp2";
-        PackageDisplayName      = "Your App2";
-        PublisherName           = "CN=YourCompany";                                         ## This must be the same as the code signing certificate.
-        PublisherDisplayName    = "YourCompany";
-        PackageVersion          = "1.0.0.0"
-        Installers = @{
-            InstallerPath       = "Path\To\Your\Installer\YourInstaller2.msi";
-#            InstallerArguements = ""                                                       ## Optional - If Installer is MSI otherwise required.
-#       $SavePackagePath = [System.IO.Path]::Combine($PSScriptRoot, "out\MSIX");            ## Optional - Working Directory will be used if not provided.
-#       $SaveTemplatePath - [System.IO.Path]::Combine($PSScriptRoot, "out\MPT_Templates");  ## Optional - Working Directory will be used if not provided.
-   }
+        PackageName          = "YourApp2";
+        PackageDisplayName   = "Your App2";
+        PublisherName        = "CN=YourCompany";    
+        PublisherDisplayName = "YourCompany";
+        PackageVersion       = "1.0.0.0";
+        InstallerPath        = "Path\to\YourInstaller2.exe";
+        InstallerArguments   = "\Silent"            
+    }#,
+#    @{
+#        PackageName          = "YourApp3";                                          ## Package File Name (No spaces or special characters)
+#        PackageDisplayName   = "Your App3";                                         ## The name of the Application
+#        PublisherName        = "CN=YourCompany";                                    ## This must be the same as the code signing certificate.
+#        PublisherDisplayName = "YourCompany";                                       ## Application Publisher Name
+#        PackageVersion       = "1.0.0.0";                                           ## Application version (quad-octet)
+#        InstallerPath        = "Path\to\YourInstaller3.exe";                        ## File Path to the Installation media
+#        InstallerArguments   = "";                                                  ## Required If Installer is not an MSI.
+#        SavePackagePath      = $([System.IO.Path]::Combine($PSScriptRoot, "out"));  ## Optional - Working Directory will be used if not provided.
+#        SaveTemplatePath     = $([System.IO.Path]::Combine($PSScriptRoot, "out"))   ## Optional - Working Directory will be used if not provided.
+#   }
 )
 
 ## Converts the identified applications to MSIX Packaging Format.
+Write-Host "`n###########  Packaging Applications  ###########" -BackgroundColor Black
 RunConversionJobs -conversionsParameters $conversionsParameters -virtualMachines $virtualMachines -remoteMachines $remoteMachines $workingDirectory
 
 ## Signs the previously created MSIX Apps with provided certificate.
-Set-MSIXSignApp -msixFolder "$workingDirectory\MSIX" -CertificatePath $SigningCertificate.Path -CertificatePassword $SigningCertificate.Password
+Write-Host "`n############  Signing Applications  ############" -BackgroundColor Black
+Set-MSIXSignApp -conversionsParameters $conversionsParameters -WorkingDirectory $workingDirectory -CertificatePath $($SigningCertificate.Path) -CertificatePassword $($SigningCertificate.Password)
