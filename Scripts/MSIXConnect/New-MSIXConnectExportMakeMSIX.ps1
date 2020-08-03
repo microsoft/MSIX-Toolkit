cec04b0801f83ff26f17cfaaf68e868d75b36043
RDMacLachlan <59626501+RDMacLachlan@users.noreply.github.com>
2020-08-03 15:32:26 -0700

    Update New-MSIXConnectExportMakeMSIX.ps1
    Removed the variable values used in testing.

--- Scripts/MSIXConnect/New-MSIXConnectExportMakeMSIX.ps1
+++ Scripts/MSIXConnect/New-MSIXConnectExportMakeMSIX.ps1
@@ -18,47 +18,32 @@ $workingDirectory = [System.IO.Path]::Combine($($PSScriptRoot), "out")
 New-LogEntry -LogValue "Collecting credentials for accessing the Remote / Virtual Machines" -Component "entry.ps1"
 $credential = Get-Credential
 
-# ## The Code Signing certificate to be used for signing the MSIX Apps.
-# $SigningCertificate = @{
-#     Password = "P@ssw0rd"; Path = "C:\Temp\cert.pfx"
-# }
-#
-# ## Virtual Machines to be used for converting applications to the MSIX Packaging format.
-# $virtualMachines = @(
-#     @{ Name = "vm1"; Credential = $credential }
-#     @{ Name = "vm2"; Credential = $credential }
-# )
-#
-# ## Remote Machines to be used for converting applications to the MSIX Packaging format.
-# $remoteMachines = @(
-#     @{ ComputerName = "YourVMNameHere.westus.cloudapp.azure.com"; Credential = $credential }
-# )
 
-## The Code Signing certificate to be used for signing the MSIX Apps.
+$CertPassword  = "[Cert Password]"
+$CertPath      = "[Path to PFX Signing Cert]"
+$CertPublisher = $(Get-PfxData -FilePath $($CertPath) -Password $($(ConvertTo-SecureString -String $($CertPassword) -AsPlainText -force))).EndEntityCertificates.Subject
+
 $SigningCertificate = @{
-    Password = "MSIX!Lab1809"; Path = "C:\Temp\Certs\msix-lab-cert.pfx"
+    Password = $CertPassword; Path = $CertPath; Publisher = $CertPublisher
 }
 
 ## Virtual Machines to be used for converting applications to the MSIX Packaging format.
 $virtualMachines = @(
-    @{ Name = "MSIX Packaging Tool Environment"; Credential = $credential }
-#    @{ Name = "MSIX Packaging Tool Environment 1"; Credential = $credential }
-#    @{ Name = "vm2"; Credential = $credential }
+    @{ Name = "[Hyper-V VM Name]"; Credential = $credential }
 )
 
 ## Remote Machines to be used for converting applications to the MSIX Packaging format.
 $remoteMachines = @(
-    @{ Name = "DESKTOP-VQQKQF9"; Credential = $credential }
+    @{ Name = "[ComputerName]"; Credential = $credential }
 )
 
+
 $conversionsParameters = @()
-#$conversionsParameters = Get-CMExportAppData -CMAppContentPath $ExportedCMAppsPath -CMAppMetaDataPath $ExportedCMAppMetaData
-$conversionsParameters = Get-CMExportAppData -CMAppParentPath $CMAppParentPath
+$conversionsParameters = Get-CMExportAppData -CMSiteCode "[ConfigMgr 3 Character Site Code]" -CMSiteServer "[ConfigMgr Site Server Name]" -AppName "[App Name - Optional]" -SigningCertificate $SigningCertificate
 
 ## Converts the identified applications to MSIX Packaging Format.
 Write-Host "`n###########  Packaging Applications  ###########" -BackgroundColor Black
-#RunConversionJobs -conversionsParameters $conversionsParameters -virtualMachines $virtualMachines -remoteMachines $remoteMachines $workingDirectory
-RunConversionJobsVMLocal -conversionsParameters $conversionsParameters $remoteMachines $virtualMachines $workingDirectory
+RunConversionJobs -conversionsParameters $conversionsParameters -virtualMachines $virtualMachines -remoteMachines $remoteMachines $workingDirectory
 
 ## Signs the previously created MSIX Apps with provided certificate.
 Write-Host "`n############  Signing Applications  ############" -BackgroundColor Black
